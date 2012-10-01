b96cf77d079e2e22ee515fdd35ac02782aceb3f8
liedtke <liedtke@a9c2e26e-c794-4387-a593-6d549c9706ce>
2012-10-01 20:15:15 +0000

    last changes

--- contest/game/game.cpp
+++ contest/game/game.cpp
@@ -174,7 +174,7 @@ bool Omove = false;
 bool ORotate = false;
 bool OScale = false;
 size_t Oindex = 2;
-bool useDeveloperFeatures = true;
+bool useDeveloperFeatures = false;
 
 
 CHAR									g_SkyboxPath[MAX_PATH] = {'\0'};
@@ -939,7 +939,7 @@ HRESULT CALLBACK OnD3D11CreateDevice( ID3D11Device* pd3dDevice,
 	screenTex_Desc.ArraySize = 1;
 	screenTex_Desc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
 	screenTex_Desc.SampleDesc.Count = 4;
-	screenTex_Desc.SampleDesc.Quality = 0;
+	screenTex_Desc.SampleDesc.Quality = 1;
 	screenTex_Desc.Usage = D3D11_USAGE_DEFAULT;
 	screenTex_Desc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
 	screenTex_Desc.CPUAccessFlags = 0;
